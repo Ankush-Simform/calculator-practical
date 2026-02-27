f256ca80414f86979a3171175fed345aa1651016
ankush dubey <ankush.dubey@simform.dom>
2026-02-27 19:35:06 +0530

    calculator-ui

---
+++ script.js
@@ -0,0 +1,32 @@
+    function getDB() {
+  return JSON.parse(localStorage.getItem("products")) || [];
+}
+
+function saveDB(data) {
+  localStorage.setItem("products", JSON.stringify(data));
+}
+
+// function add(){
+    
+// }
+// function sub(){
+
+// }
+// function mul(){
+
+// }
+// function div(){
+
+// }
+// function
+// switch (basic){
+//     case '+':
+//         result=number1+number2;
+//         console.log(`${number1} + ${number2}=${result}`);
+//         break;
+// }
+const inputField=document.getElementById()
+function displayData(e){
+
+    document.getElementById('input').addEventListener('click',)=e.trarget.value;
+}   
